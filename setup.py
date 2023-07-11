c9f8f59b8dff7bed298ee527661ab96557c5dadf
Fabio Uechi <308613+fabito@users.noreply.github.com>
2023-07-12 11:14:33 +1200

    Attempt to make pysot installable from pip

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
-from distutils.core import setup
+import os
 from distutils.extension import Extension
-from Cython.Build import cythonize
+# from Cython.Build import cythonize
+from setuptools import find_packages, setup
+
+
+def find_yaml_files():
+    yaml_files = []
+    for root, dirs, files in os.walk("experiments"):
+        for file in files:
+            if file.endswith(".yaml"):
+                yaml_files.append(os.path.join(root, file))
+    print(yaml_files)
+    return yaml_files
+
 
 ext_modules = [
     Extension(
@@ -15,8 +27,14 @@ ext_modules = [
     )
 ]
 
+
 setup(
-    name='toolkit',
-    packages=['toolkit'],
-    ext_modules=cythonize(ext_modules)
+    name='pysot',
+    version='0.0.1',
+    python_requires='>=3.7',
+    packages=find_packages(),
+    data_files=[("configs", find_yaml_files())],
+    include_package_data=True,
+    # ext_modules=cythonize(ext_modules),
+    platforms=["linux"],
 )
