ac1e5a5582efd740ebb712eb2482fc7fb12a2c72
Fabio Uechi <308613+fabito@users.noreply.github.com>
2023-07-12 14:08:40 +1200

    Include yamls in package

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import os
-from distutils.extension import Extension
-# from Cython.Build import cythonize
 from setuptools import find_packages, setup
-
 
 def find_yaml_files():
     yaml_files = []
@@ -13,33 +10,16 @@ def find_yaml_files():
     print(yaml_files)
     return yaml_files
 
-
-ext_modules = [
-    Extension(
-        name='toolkit.utils.region',
-        sources=[
-            'toolkit/utils/region.pyx',
-            'toolkit/utils/src/region.c',
-        ],
-        include_dirs=[
-            'toolkit/utils/src'
-        ]
-    )
-]
-
-
 setup(
     name='pysot',
     version='0.0.1',
-    python_requires='>=3.8',
+    python_requires='>=3.8  ',
     packages=find_packages(),
-    data_files=[("configs", find_yaml_files())],
     include_package_data=True,
     install_requires=[
         "yacs",
         "opencv-python-headless",
         "numpy"
     ],
-    # ext_modules=cythonize(ext_modules),
     platforms=["linux"],
 )
